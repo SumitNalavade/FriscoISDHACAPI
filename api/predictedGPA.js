35cadb98f650b14917534c43e2ea9aec97119905
SumitNalavade <vs.nalavade2003@gmail.com>
2022-02-15 13:26:33 -0600

    Add CORs

--- api/predictedGPA.js
+++ api/predictedGPA.js
@@ -1,12 +1,10 @@
 const { default: axios } = require("axios");
-const { micro } = require("micro-cors");
 
-async function predictedGPA(req, res) {
-    if (req.method === "OPTIONS") {
-      return res.status(200).end();
+module.exports = async (req, res) => {
+    if (req.method === 'OPTIONS') {
+        return response.status(200).send('ok');
     }
-    
-    res.setHeader('Access-Control-Allow-Origin', '*');
+
     const { weightedGPA, unweightedGPA, studentGrade, currentClasses } = req.body;
 
     const { data } = await axios.post(`https://gradualgrades.herokuapp.com/predictedGPA`, {
@@ -17,8 +15,4 @@ async function predictedGPA(req, res) {
     });
 
     return res.send(data);
-  }
-
-const cors = micro();
-
-export default cors(MyApi);
+}
