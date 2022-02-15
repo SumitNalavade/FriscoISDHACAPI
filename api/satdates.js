8fb161727a0a3a04635bd64ad6d6cfe736c087a0
SumitNalavade <vs.nalavade2003@gmail.com>
2022-02-15 13:40:49 -0600

    Revert TO AWS

--- api/satdates.js
+++
@@ -1,8 +0,0 @@
-const { default: axios } = require("axios");
-
-module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    const { data } = await axios.get("https://gradualgrades.herokuapp.com/satdates");
-
-    return res.send(data);
-}
