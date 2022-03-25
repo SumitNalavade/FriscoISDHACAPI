7be8eb0d311debe006ef1c00943fc25e18690384
SumitNalavade <vs.nalavade2003@gmail.com>
2022-03-24 23:26:12 -0500

    Add past assignment data route

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,8 @@
 from crypt import methods
 from flask import Flask, request
 from flask_cors import CORS
-from index import (getGPAS, getInfo, getCurrentClasses, predictGPA, getSATDates, getStudentSchedule)
+from index import (getGPAS, getInfo, getCurrentClasses, getPast,
+                   predictGPA, getSATDates, getPast, getStudentSchedule)
 from fakeData import *
 
 application = Flask(__name__)
@@ -10,6 +11,34 @@ CORS(application)
 @application.route("/", methods=["GET"])
 def home():
     return "Hello World"
+
+@application.route("/students/pastassignments", methods=["GET"])
+def pastAssignments():
+    username = request.args.get("username")
+    password = request.args.get("password")
+    quarter = request.args.get("quarter")
+
+
+    if(username.lower() == "john" and password.lower() == "doe"):
+        return currentClasses
+
+    courses = []
+
+    classes = getPast(username, password, quarter)
+
+    for course in classes:
+        courses.append(
+            {
+                "name": course.name,
+                "grade": course.grade,
+                "weight": course.weight,
+                "credits": course.credits,
+                "Last Updated": course.updateDate,
+                "assignments": course.assignments
+            }
+        )
+
+    return {"currentClasses": courses}
 
 @application.route("/students/gpa", methods=["GET"])
 def sendGPAS():
@@ -21,14 +50,14 @@ def sendGPAS():
 
     return getGPAS(username, password)
 
+
 @application.route("/students/info", methods=["GET"])
 def sendInfo():
     username = request.args.get("username")
     password = request.args.get("password")
 
-
     if(username.lower() == "john" and password.lower() == "doe"):
-            return studentData
+        return studentData
 
     return getInfo(username, password)
 
@@ -39,9 +68,10 @@ def sendSchedule():
     password = request.args.get("password")
 
     if(username.lower() == "john" and password.lower() == "doe"):
-            return schedule
+        return schedule
 
     return {"schedule": getStudentSchedule(username, password)}
+
 
 @application.route("/students/currentclasses", methods=["GET"])
 def sendCurrentClasses():
@@ -49,25 +79,26 @@ def sendCurrentClasses():
     password = request.args.get("password")
 
     if(username.lower() == "john" and password.lower() == "doe"):
-            return currentClasses
+        return currentClasses
 
     courses = []
 
     classes = getCurrentClasses(username, password)
-    
+
     for course in classes:
         courses.append(
             {
-                "name" : course.name,
-                "grade" : course.grade,
-                "weight" : course.weight,
-                "credits" : course.credits,
-                "Last Updated" : course.updateDate,
-                "assignments" : course.assignments
+                "name": course.name,
+                "grade": course.grade,
+                "weight": course.weight,
+                "credits": course.credits,
+                "Last Updated": course.updateDate,
+                "assignments": course.assignments
             }
         )
 
     return {"currentClasses": courses}
+
 
 @application.route("/predictedGPA", methods=["POST"])
 def sendPredictedGPA():
@@ -78,8 +109,9 @@ def sendPredictedGPA():
 
     return (predictGPA(weightedGPA, unweightedGPA, studentGrade, currentClasses))
 
+
 @application.route("/satdates", methods=["GET"])
 def sendSATDates():
     return {
-        "dates" : getSATDates()
+        "dates": getSATDates()
     }
--- index.py
+++ index.py
@@ -2,7 +2,73 @@ from course import Course
 from utils import *
 import requests
 from AppError import AppError
+    
+def getPast(username, password, quarter):
+    try:
+        courses = []
 
+        classesDOM = getPastAssignments(username, password, quarter)
+
+        parser = createBS4Parser(classesDOM.text)
+        classContainer = parser.find_all("div", "AssignmentClass")
+
+        for container in classContainer:
+            parser = createBS4Parser(f"<html><body>{container}</body></html>")
+            headerContainer = parser.find_all("div", "sg-header sg-header-square")
+            assignementsContainer = parser.find_all("div", "sg-content-grid")
+
+            newCourse = Course("", "", "", "", "", [])
+
+            for hc in headerContainer:
+                parser = createBS4Parser(f"<html><body>{hc}</body></html>")
+                newCourse.name = parser.find("a", "sg-header-heading").text.strip()
+                newCourse.updateDate = parser.find("span", "sg-header-sub-heading").text.strip().replace("(Last Updated: ", "").replace(")", "")
+                newCourse.grade = parser.find("span", "sg-header-heading sg-right").text.strip().replace("Student Grades ", "").replace("%", "")
+            
+                if("ap" in newCourse.name.lower()):
+                    newCourse.weight = "6"
+                elif("ism" in newCourse.name.lower() or "academic dec" in newCourse.name.lower() or "adv" in newCourse.name.lower()):
+                    newCourse.weight = "5.5"
+                else:
+                    newCourse.weight = "5"
+
+                for name in doubleWeighted:
+                    if(name in newCourse.name.lower()):
+                        newCourse.credits = "2"
+                    else:
+                        newCourse.credits = "1"
+                
+            for ac in assignementsContainer:
+                parser = createBS4Parser(f"<html><body>{ac}</body></html>")
+                rows = parser.find_all("tr", "sg-asp-table-data-row")
+                for assignmentContainer in rows:
+                    try:    
+                        parser = createBS4Parser(f"<html><body>{assignmentContainer}</body></html>")
+                        tds = parser.find_all("td")
+                        assignmentName = parser.find("a").text.strip()
+                        assignmentDateDue = tds[0].text.strip()
+                        assignmentDateAssigned = tds[1].text.strip()
+                        assignmentCategory = tds[3].text.strip()
+                        assignmentScore = tds[4].text.strip()
+                        assignmentTotalPoints = tds[5].text.strip()
+
+                        newCourse.assignments.append(
+                            {
+                                "dateDue": assignmentDateDue,
+                                "dateAssigned": assignmentDateAssigned,
+                                "assignment": assignmentName,
+                                "category": assignmentCategory,
+                                "score": assignmentScore,
+                                "totalPoints" : assignmentTotalPoints
+                            }
+                        )         
+                    except:
+                        pass
+            courses.append(newCourse)
+        
+        return courses
+    except:
+        return AppError(500, "Failed to get current classes")
 
 #Get current student GPAs from their transcript
 def getGPAS(username, password):
