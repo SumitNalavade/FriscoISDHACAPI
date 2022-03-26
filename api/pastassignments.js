72baaf60b5b67b96c0312f76e68c736695477ef8
SumitNalavade <vs.nalavade2003@gmail.com>
2022-03-25 19:09:51 -0500

    Fix past assignments

--- api/pastassignments.js
+++ api/pastassignments.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     const { username, password, quarter } = req.query;
 
-    const { data } = await axios.get(`https://gradualgrades.herokuapp.com/students/currentclasses?username=${username}&password=${password}&quarter=${quarter}`);
+    const { data } = await axios.get(`https://gradualgrades.herokuapp.com/students/pastassignments?username=${username}&password=${password}&quarter=${quarter}`);
 
     return res.send(data)
 
