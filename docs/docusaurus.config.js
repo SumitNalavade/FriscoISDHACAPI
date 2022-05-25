06f314bd5dc8c8e22647d164900fbe6adf14458e
SumitNalavade <vs.nalavade2003@gmail.com>
2022-05-25 15:53:08 -0500

    configure docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'SumitNalavade', // Usually your GitHub org/user name.
+  projectName: 'FriscoISDHACAPI', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -49,11 +49,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: 'Frisco ISD HAC API',
         items: [
           {
             type: 'doc',
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -29,11 +29,11 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+       
       </main>
     </Layout>
   );
