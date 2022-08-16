f665281881828c070cfafac33798360715ed014f
SumitNalavade <vs.nalavade2003@gmail.com>
2022-08-16 01:48:28 -0500

    revert go

---
+++ api/pastassignments.js
@@ -0,0 +1,12 @@
+const { default: axios } = require("axios");
+
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const { username, password, quarter } = req.query;
+
+    const { data } = await axios.get(`https://gradualgrades.herokuapp.com/students/pastassignments?username=${username}&password=${password}&quarter=${quarter}`);
+
+    return res.send(data)
+
+    return res.status(500).send(error.response.data)
+}
--- utils/findFromDoc.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "github.com/PuerkitoBio/goquery"
-
-func FindFromDoc(doc *goquery.Document, identifier string) *goquery.Selection {
-	return doc.Find(identifier)
-}
