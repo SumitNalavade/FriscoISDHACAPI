67a15b348518078545091c8123e031599719aa7e
SumitNalavade <vs.nalavade2003@gmail.com>
2022-08-29 03:15:44 -0500

    Add currentclasses route

--- api/currentclasses.go
+++ api/currentclasses.go
@@ -1,35 +1,59 @@
 package handler
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 
 	"github.com/SumitNalavade/FRISCOISDHACAPIV2/utils"
-	"github.com/go-resty/resty/v2"
 )
 
 func CurrentClassesHandler(w http.ResponseWriter, r *http.Request) {
-	var restyClient = resty.New()
+	var courses []utils.StudentCourseType
 
 	queryParams := r.URL.Query()
 
 	username := queryParams.Get("username")
 	password := queryParams.Get("password")
 
-	if username == "john" && password == "doe" {
-		response, _ := json.Marshal(utils.FakeStudentCurrentClasses)
-		w.Header().Add("Content-Type", "application/json")
-		fmt.Fprintf(w, string(response))
-		return
-	}
+	pageContent := utils.GetPageContent(username, password, utils.CLASSESURL)
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(pageContent))
 
-	apiURL := fmt.Sprintf("https://gradualgrades.herokuapp.com/students/currentclasses?username=%v&password=%v", username, password)
+	doc.Find(".AssignmentClass").Each(func(i int, s *goquery.Selection) {
+		var newCourse utils.StudentCourseType
 
-	apiResponse, _ := restyClient.R().
-			EnableTrace().
-			Get(apiURL)
+		newCourse.Name = strings.TrimSpace(s.Find("a.sg-header-heading").Text())
+		newCourse.Grade = strings.TrimSpace(s.Find("span.sg-header-heading").Text())
+		newCourse.LastUpdated = strings.Replace(strings.Replace(strings.TrimSpace(s.Find(".sg-header-sub-heading").Text()), "(Last Updated: ", "", 1), ")", "", 1)
 
-	w.Header().Add("Content-Type", "application/json") 
-	fmt.Fprintf(w, apiResponse.String())
+		s.Find(".sg-asp-table-data-row").Each(func(i int, s *goquery.Selection) {
+			var newAssignment utils.StudentAssignmentType
+
+			s.Find("td").Each(func(i int, s *goquery.Selection) {
+				text := strings.TrimSpace(s.Text())
+				switch i {
+				case 0:
+					newAssignment.DateDue = text
+				case 1:
+					newAssignment.DateAssigned = text
+				case 2:
+					newAssignment.Name = strings.TrimSpace(strings.Replace(text, "*", "", 1))
+				case 3:
+					newAssignment.Category = text
+				case 4:
+					newAssignment.Score = text
+				case 5:
+					newAssignment.TotalPoints = text
+				}
+			})
+			newCourse.Assignments = append(newCourse.Assignments, newAssignment)
+		})
+
+		courses = append(courses, newCourse)
+	})
+
+	fmt.Fprintf(w, "success")
 }
--- utils/resources.go
+++ utils/resources.go
@@ -3,3 +3,4 @@ package utils
 const TRANSCRIPTURL string = "https://hac.friscoisd.org/HomeAccess/Content/Student/Transcript.aspx"
 const REGISTRATIONURL string = "https://hac.friscoisd.org/HomeAccess/Content/Student/Registration.aspx"
 const SCHEDULEURL string = "https://hac.friscoisd.org/HomeAccess/Content/Student/Classes.aspx"
+const CLASSESURL string = "https://hac.friscoisd.org/HomeAccess/Content/Student/Assignments.aspx"
