24f4285fae253114e4c1bef572dc7e3a9d46085f
SumitNalavade <vs.nalavade2003@gmail.com>
2022-12-24 20:04:40 -0600

    Update API Homepage

--- api/index.go
+++ api/index.go
@@ -9,6 +9,6 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, `
 		<h1>Frisco ISD HAC API</h1>
 		<p>Documentation:</p>
-		<a href="https://friscoisdhacapidocs.vercel.app/">friscoisdhacapidocs.vercel.app</a>
+		<a href="https://friscoisdhacapidocs.vercel.app/" target="_blank">friscoisdhacapidocs.vercel.app</a>
 	`)
 }
