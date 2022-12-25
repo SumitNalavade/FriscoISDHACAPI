a6bd8377d0c84278d69b0cdb8b12af760c9f11f2
SumitNalavade <vs.nalavade2003@gmail.com>
2022-12-25 12:11:04 -0600

    Remove Vercelignore

--- api/index.go
+++ api/index.go
@@ -8,7 +8,7 @@ import (
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, `
 		<h1>Frisco ISD HAC API</h1>
-		<p>API Documentation:</p>
+		<p>Documentation:</p>
 		<a href="https://friscoisdhacapidocs.vercel.app/" target="_blank">friscoisdhacapidocs.vercel.app</a>
 	`)
 }
