48c78e82dc1df56febf1928b728df4fabcb41dab
SumitNalavade <vs.nalavade2003@gmail.com>
2022-12-30 15:08:06 -0600

    Restore homepage

--- api/index.go
+++ api/index.go
@@ -6,15 +6,9 @@ import (
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-
-	IPAddress := r.Header.Get("X-Real-Ip")
-
-	fmt.Fprintf(w, IPAddress)
-
-
-	// fmt.Fprint(w, `
-	// 	<h1>Frisco ISD HAC API</h1>
-	// 	<p>Documentation:</p>
-	// 	<a href="https://friscoisdhacapidocs.vercel.app/" target="_blank">https://friscoisdhacapidocs.vercel.app/</a>
-	// `)
+	fmt.Fprint(w, `
+		<h1>Frisco ISD HAC API</h1>
+		<p>Documentation:</p>
+		<a href="https://friscoisdhacapidocs.vercel.app/" target="_blank">https://friscoisdhacapidocs.vercel.app/</a>
+	`)
 }
