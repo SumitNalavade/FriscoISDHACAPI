8fee3ba25ff5386baae541b756e4cca01685460b
SumitNalavade <vs.nalavade2003@gmail.com>
2023-01-05 00:38:10 -0600

    Update README and API Homepage

---
+++ api/index.py
@@ -0,0 +1,18 @@
+from http.server import BaseHTTPRequestHandler
+
+from _lib.getRequestSession import getRequestSession
+
+class handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    print(f"REQUEST IP: {self.client_address[0]}")
+
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+    self.wfile.write("""
+    <h1>Frisco ISD HAC API</h1>
+	<p>Documentation:</p>
+	<a href="https://friscoisdhacapi.vercel.app/" target="_blank">https://friscoisdhacapi.vercel.app/</a>
+    """.encode(encoding="utf_8"))
+
+    return
